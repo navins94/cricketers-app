914a82f6c5798379c24fb3301a4cc353d0bba386
navins94 <nikshingote@gmail.com>
2023-06-29 15:47:42 +0530

    Fix lint issues

--- src/components/UI/atoms/Link.tsx
+++ src/components/UI/atoms/Link.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from "react";
+import { FC, ReactNode } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { Link as MuiLink } from "@mui/material";
 
--- src/components/UI/atoms/Spinner.tsx
+++ src/components/UI/atoms/Spinner.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 
 const SpinnerComponent: FC = () => {
